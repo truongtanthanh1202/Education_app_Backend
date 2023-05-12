cb7672f5a176f68811592c70dffd2fb12e7b2912
Tran Quy Manh - 21020352 <21020352@vnu.edu.vn>
2023-05-12 12:02:46 +0700

    update forgor password with new email server

--- src/app/controller/Emailcontroller.js
+++ src/app/controller/Emailcontroller.js
@@ -3,160 +3,163 @@ const {mongooseToObject} = require('../../util/mongoose');
 const Student = require('../model/User/Student/Student');
 const Teacher = require('../model/User/Teacher/Teacher');
 class EmailController {
-  async sendEmail(req, res) {
-    try {
-      const email = req.body.email;
-      const student = await Student.findOne({email: email});
-      if (student === null) {
-        const teacher = await Teacher.findOne({email: email});
-        if (teacher === null) {
-          res.redirect('back');
+    async sendEmail(req, res) {
+        try {
+            const email = req.body.email;
+            const student = await Student.findOne({email: email});
+            if (student === null) {
+                const teacher = await Teacher.findOne({email: email});
+                if (teacher === null) {
+                    res.redirect('back');
+                } else {
+                    let otp = [];
+                    for (let i = 0; i < 6; i++) {
+                        let code = Math.floor(Math.random() * 9);
+                        otp.push(code);
+                    }
+                    await mailer.sendMail(
+                        email,
+                        'Reset Password',
+                        `Your otp code is: ${otp.join('')}`,
+                    );
+                    await Teacher.findOneAndUpdate(
+                        {email: email},
+                        {$set: {otp: otp.join('')}},
+                        {upsert: true},
+                    );
+                    res.render('login/verifyOTP', {
+                        user: mongooseToObject(teacher),
+                    });
+                }
+            } else {
+                let otp = [];
+                for (let i = 0; i < 6; i++) {
+                    let code = Math.floor(Math.random() * 9);
+                    otp.push(code);
+                }
+                await mailer.sendMail(
+                    email,
+                    'Reset Password',
+                    `Your otp code is: ${otp.join('')}`,
+                );
+                const test = await Student.findOneAndUpdate(
+                    {email: email},
+                    {$set: {otp: otp.join('')}},
+                    {upsert: true},
+                );
+                res.render('login/verifyOTP', {
+                    user: mongooseToObject(student),
+                });
+            }
+        } catch (error) {
+            res.json(error);
+        }
+    }
+
+    async sendOTP(req, res) {
+        try {
+            const email = req.body.email;
+            const student = await Student.findOne({email: email});
+            if (student === null) {
+                const teacher = await Teacher.findOne({email: email});
+                if (teacher === null) {
+                    res.json({message: '400'});
+                } else {
+                    let otp = [];
+                    for (let i = 0; i < 6; i++) {
+                        let code = Math.floor(Math.random() * 9);
+                        otp.push(code);
+                    }
+                    try {
+                        await mailer.sendMail(
+                            email,
+                            'Reset Password',
+                            `Your otp code is: ${otp.join('')}`,
+                        );
+                    } catch (error) {
+                        res.json({message: '400'});
+                    }
+
+                    await Teacher.updateOne(
+                        {email: email},
+                        {$set: {otp: otp.join('')}},
+                        {upsert: true},
+                    );
+                    res.json({
+                        message: '200',
+                    });
+                }
+            } else {
+                let otp = [];
+                for (let i = 0; i < 6; i++) {
+                    let code = Math.floor(Math.random() * 9);
+                    otp.push(code);
+                }
+                await mailer.sendMail(
+                    email,
+                    'Reset Password',
+                    `Your otp code is: ${otp.join('')}`,
+                );
+                await Student.updateOne(
+                    {email: email},
+                    {$set: {otp: otp.join('')}},
+                    {upsert: true},
+                );
+                res.json({
+                    message: '200',
+                });
+            }
+        } catch (error) {
+            res.json({message: '400'});
+        }
+    }
+
+    verifyOTP(req, res) {
+        res.render('login/verifyOTP');
+    }
+
+    async checkOTP(req, res) {
+        const student = await Student.findOne({
+            email: req.params.slug,
+            otp: req.body.otp.join(''),
+        });
+        if (student === null) {
+            const teacher = await Teacher.findOne({
+                email: req.params.slug,
+                otp: req.body.otp.join(''),
+            });
+            if (teacher === null) {
+                res.send('please fill again!');
+            } else {
+                res.render('login/resetPassword', {
+                    user: mongooseToObject(teacher),
+                });
+            }
         } else {
-          let otp = [];
-          for (let i = 0; i < 6; i++) {
-            let code = Math.floor(Math.random() * 9);
-            otp.push(code);
-          }
-          await mailer.sendMail(
-            email,
-            'Reset Password',
-            `Your otp code is: ${otp.join('')}`,
-          );
-          await Teacher.findOneAndUpdate(
-            {email: email},
-            {$set: {otp: otp.join('')}},
-            {upsert: true},
-          );
-          res.render('login/verifyOTP', {
-            user: mongooseToObject(teacher),
-          });
+            res.render('login/resetPassword', {
+                user: mongooseToObject(student),
+            });
         }
-      } else {
-        let otp = [];
-        for (let i = 0; i < 6; i++) {
-          let code = Math.floor(Math.random() * 9);
-          otp.push(code);
-        }
-        await mailer.sendMail(
-          email,
-          'Reset Password',
-          `Your otp code is: ${otp.join('')}`,
-        );
-        const test = await Student.findOneAndUpdate(
-          {email: email},
-          {$set: {otp: otp.join('')}},
-          {upsert: true},
-        );
-        res.render('login/verifyOTP', {
-          user: mongooseToObject(student),
-        });
-      }
-    } catch (error) {
-      res.json(error);
     }
-  }
 
-  async sendOTP(req, res) {
-    try {
-      const email = req.body.email;
-      const student = await Student.findOne({email: email});
-      if (student === null) {
-        const teacher = await Teacher.findOne({email: email});
-        if (teacher === null) {
-          res.json({message: 'Email do not exist!'});
+    async checkOTPofficial(req, res) {
+        const student = await Student.findOne({
+            email: req.body.email,
+            otp: req.body.otp,
+        });
+        if (student === null) {
+            const teacher = await Teacher.findOne({
+                email: req.body.email,
+                otp: req.body.otp,
+            });
+            if (teacher === null) {
+                res.json({message: 'OTP code is not correct!'});
+            } else {
+                res.json({message: 'OTP code is correct!'});
+            }
         } else {
-          let otp = [];
-          for (let i = 0; i < 6; i++) {
-            let code = Math.floor(Math.random() * 9);
-            otp.push(code);
-          }
-          await mailer.sendMail(
-            email,
-            'Reset Password',
-            `Your otp code is: ${otp.join('')}`,
-          );
-          await Teacher.findOneAndUpdate(
-            {email: email},
-            {$set: {otp: otp.join('')}},
-            {upsert: true},
-          );
-          res.json({
-            message:
-              'OTP code have already been sent to your email. Please check!',
-          });
+            res.json({message: 'OTP code is correct!'});
         }
-      } else {
-        let otp = [];
-        for (let i = 0; i < 6; i++) {
-          let code = Math.floor(Math.random() * 9);
-          otp.push(code);
-        }
-        await mailer.sendMail(
-          email,
-          'Reset Password',
-          `Your otp code is: ${otp.join('')}`,
-        );
-        const test = await Student.findOneAndUpdate(
-          {email: email},
-          {$set: {otp: otp.join('')}},
-          {upsert: true},
-        );
-        res.json({
-          message:
-            'OTP code have already been sent to your email. Please check!',
-        });
-      }
-    } catch (error) {
-      res.json(error);
     }
-  }
-
-  verifyOTP(req, res) {
-    res.render('login/verifyOTP');
-  }
-
-  async checkOTP(req, res) {
-    const student = await Student.findOne({
-      email: req.params.slug,
-      otp: req.body.otp.join(''),
-    });
-    if (student === null) {
-      const teacher = await Teacher.findOne({
-        email: req.params.slug,
-        otp: req.body.otp.join(''),
-      });
-      if (teacher === null) {
-        res.send('please fill again!');
-      } else {
-        res.render('login/resetPassword', {
-          user: mongooseToObject(teacher),
-        });
-      }
-    } else {
-      res.render('login/resetPassword', {
-        user: mongooseToObject(student),
-      });
-    }
-  }
-
-  async checkOTPofficial(req, res) {
-    const student = await Student.findOne({
-      email: req.body.email,
-      otp: req.body.otp,
-    });
-    if (student === null) {
-      const teacher = await Teacher.findOne({
-        email: req.body.email,
-        otp: req.body.otp,
-      });
-      if (teacher === null) {
-        res.json({message: 'OTP code is not correct!'});
-      } else {
-        res.json({message: 'OTP code is correct!'});
-      }
-    } else {
-      res.json({message: 'OTP code is correct!'});
-    }
-  }
 }
 module.exports = new EmailController();
